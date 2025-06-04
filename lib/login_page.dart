14c0d7ae2f4c518861ba788a743360b2d3476d37
Aa-05-di <aadithyanpramad@gmail.com>
2025-06-04 16:11:59 +0400

    first

---
+++ lib/login_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[100],
+        toolbarHeight: 90,
+        elevation: 0,
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.only(top:50.0),
+          child: ShaderMask(
+            shaderCallback: (bounds) => LinearGradient(
+              colors: [Colors.indigo, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ).createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
+            child: Text(
+              "E-COMMERCE",
+              style: TextStyle(
+                fontSize: 46,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                letterSpacing: 2.0,
+                color: Colors.white, // Required for ShaderMask to show gradient
+                shadows: [
+                  Shadow(
+                    blurRadius: 2,
+                    color: Colors.black26,
+                    offset: Offset(1, 1),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 90),
+              //Username
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  color: Colors.deepPurple[50],
+                  elevation: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 13,
+                      vertical: 12,
+                    ),
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.black38,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Username',
+                        hintStyle: TextStyle(color: Colors.deepPurple),
+                        icon: Icon(Icons.person, color: Colors.deepPurple),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              //Password
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(20),
+                  ),
+                  elevation: 3,
+                  color: Colors.deepPurple[50],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 13,
+                      vertical: 12,
+                    ),
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.black38,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(Icons.key, color: Colors.deepPurple),
+                        hintText: "Password",
+                        hintStyle: TextStyle(color: Colors.deepPurple),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              //LOGIN
+              SizedBox(
+                width: 100,
+                height: 50,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10),
+                  ),
+                  color: Colors.deepPurple[50],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 22,
+                      vertical: 11,
+                    ),
+                    child: Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 60),
+              //Register button
+              GestureDetector(
+                child: Text(
+                  "Not a User? Register Now",
+                  style: TextStyle(color: Colors.deepPurple),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

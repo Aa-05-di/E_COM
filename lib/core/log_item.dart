44ef682e0a356752f883d029c6e80b7016c54763
Aa-05-di <aadithyanpramad@gmail.com>
2025-06-10 10:39:10 +0400

    validation

--- lib/core/log_item.dart
+++ lib/core/log_item.dart
@@ -5,7 +5,6 @@ class LoginInputField extends StatelessWidget {
   final IconData icon;
   final TextEditingController controller;
   final String? Function(String?)? validator;
-  final bool obscureText;
 
   const LoginInputField({
     super.key,
@@ -13,43 +12,64 @@ class LoginInputField extends StatelessWidget {
     required this.icon,
     required this.controller,
     required this.validator,
-    this.obscureText = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 60,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        color: Colors.deepPurple[50],
-        elevation: 3,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 5),
-          child: TextFormField(
-            controller: controller,
-            obscureText: obscureText,
-            style: const TextStyle(
-              color: Colors.black87,
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 60,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: hintText,
-              hintStyle: const TextStyle(
-                color: Colors.grey,
-                fontSize: 13,
+            color: Colors.deepPurple[50],
+            elevation: 3,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 5),
+              child: TextFormField(
+                controller: controller,
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: hintText,
+                  hintStyle: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 13,
+                  ),
+                  icon: Icon(icon, color: Colors.deepPurple),
+                ),
+                validator: null,
               ),
-              icon: Icon(icon, color: Colors.deepPurple),
             ),
-            validator: validator,
           ),
         ),
-      ),
+        if (validator != null)
+          FormField<String>(
+            validator: validator,
+            builder: (FormFieldState<String> state) {
+              return state.hasError
+                  ? Padding(
+                      padding: const EdgeInsets.only(top: 4, left: 13),
+                      child: Text(
+                        state.errorText!,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 12,
+                        ),
+                      ),
+                    )
+                  : const SizedBox.shrink();
+            },
+          ),
+      ],
     );
   }
 }
--- lib/core/reg_item.dart
+++ lib/core/reg_item.dart
@@ -4,37 +4,69 @@ class RegBox extends StatelessWidget {
   final String? Function(String?)? validator;
   final String data;
   final TextEditingController controller;
-  const RegBox({super.key,required this.data,required this.controller,required this.validator});
+
+  const RegBox({
+    super.key,
+    required this.data,
+    required this.controller,
+    required this.validator,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 50,
-      child: Card(
-        color: Colors.deepPurple[50],
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(10),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
-          child: TextFormField(
-            maxLines: 1,
-            controller: controller,
-            style: TextStyle(color: const Color.fromARGB(255, 107, 107, 107),fontSize: 10),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: data,
-              hintStyle: TextStyle(
-                fontSize: 10,
-                color: Colors.grey,
-                fontWeight: FontWeight.bold,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: Card(
+            color: Colors.deepPurple[50],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+              child: TextFormField(
+                maxLines: 1,
+                controller: controller,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 107, 107, 107),
+                  fontSize: 10,
+                ),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: data,
+                  hintStyle: const TextStyle(
+                    fontSize: 10,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                validator: null,
               ),
             ),
-            validator: validator,
           ),
         ),
-      ),
+        if (validator != null)
+          FormField<String>(
+            validator: validator,
+            builder: (FormFieldState<String> state) {
+              return state.hasError
+                  ? Padding(
+                      padding: const EdgeInsets.only(top: 4, left: 12),
+                      child: Text(
+                        state.errorText!,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 12,
+                        ),
+                      ),
+                    )
+                  : const SizedBox.shrink();
+            },
+          ),
+      ],
     );
   }
 }
